3af0c496f0e2899e89c38a71b0eccd2ebbdbc6b9
lnatarenr <lnatarenr@gmail.com>
2014-02-18 20:10:00 +0000

    cambios label administrar documentos

--- protected/views/document/admin.php
+++ protected/views/document/admin.php
@@ -5,8 +5,8 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'List Document', 'url'=>array('index')),
-	array('label'=>'Create Document', 'url'=>array('create')),
+	array('label'=>'Lista documento', 'url'=>array('index')),
+	array('label'=>'Crear documento', 'url'=>array('create')),
 );
 
 Yii::app()->clientScript->registerScript('search', "
@@ -23,7 +23,7 @@ $('.search-form form').submit(function(){
 ");
 ?>
 
-<h1>Manage Documents</h1>
+<h1>Gestionar documentos</h1>
 
 <p>
 You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
