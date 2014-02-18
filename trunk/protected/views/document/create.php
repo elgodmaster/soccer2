bdf3cbd4801c1611ce49691d922a02ef1c3ba0e1
lnatarenr@gmail.com <lnatarenr@gmail.com@9a77e7a4-2626-9e75-c391-078a5be7c4ed>
2014-02-18 22:11:49 +0000

    modificacion label crear documento

--- trunk/protected/views/document/create.php
+++ trunk/protected/views/document/create.php
@@ -5,11 +5,11 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'List Document', 'url'=>array('index')),
-	array('label'=>'Manage Document', 'url'=>array('admin')),
+	array('label'=>'Lista documento', 'url'=>array('index')),
+	array('label'=>'Gestionar documento', 'url'=>array('admin')),
 );
 ?>
 
-<h1>Create Document</h1>
+<h1>Crear documento</h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
