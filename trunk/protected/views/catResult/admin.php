4f67acb6913056368a76a4a3d2e1a166da725f7d
lnatarenr@gmail.com <lnatarenr@gmail.com@9a77e7a4-2626-9e75-c391-078a5be7c4ed>
2014-02-18 20:01:46 +0000

    cambios admin resultados

--- trunk/protected/views/catResult/admin.php
+++ trunk/protected/views/catResult/admin.php
@@ -8,8 +8,8 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'List CatResult', 'url'=>array('index')),
-	array('label'=>'Create CatResult', 'url'=>array('create')),
+	array('label'=>'Lista resultados', 'url'=>array('index')),
+	array('label'=>'Crear resultados', 'url'=>array('create')),
 );
 
 Yii::app()->clientScript->registerScript('search', "
@@ -26,7 +26,7 @@ $('.search-form form').submit(function(){
 ");
 ?>
 
-<h1>Manage Cat Results</h1>
+<h1>Gestionar resultados </h1>
 
 <p>
 You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
