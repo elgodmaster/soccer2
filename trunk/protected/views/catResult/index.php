439a1a45cf0d31834306f4eb333f3eeb806d007a
lnatarenr@gmail.com <lnatarenr@gmail.com@9a77e7a4-2626-9e75-c391-078a5be7c4ed>
2014-02-18 19:58:42 +0000

    cambios label categoria

--- trunk/protected/views/catResult/index.php
+++ trunk/protected/views/catResult/index.php
@@ -7,12 +7,12 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'Create CatResult', 'url'=>array('create')),
-	array('label'=>'Manage CatResult', 'url'=>array('admin')),
+	array('label'=>'Crear resultados ', 'url'=>array('create')),
+	array('label'=>'Gestionar resultados', 'url'=>array('admin')),
 );
 ?>
 
-<h1>Cat Results</h1>
+<h1>Resultado categorias</h1>
 
 <?php $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
