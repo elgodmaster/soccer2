f8d1025e3d6815ed3ab68a6583ad54d5c25e6195
lnatarenr <lnatarenr@gmail.com>
2014-02-19 03:00:47 +0000

    modificacion de label rol

--- protected/views/role/view.php
+++ protected/views/role/view.php
@@ -5,15 +5,15 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'List Role', 'url'=>array('index')),
-	array('label'=>'Create Role', 'url'=>array('create')),
-	array('label'=>'Update Role', 'url'=>array('update', 'id'=>$model->ID)),
-	array('label'=>'Delete Role', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->ID),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Role', 'url'=>array('admin')),
+	array('label'=>'Lista rol', 'url'=>array('index')),
+	array('label'=>'Crear rol', 'url'=>array('create')),
+	array('label'=>'Actualizar rol', 'url'=>array('update', 'id'=>$model->ID)),
+	array('label'=>'Borrar rol', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->ID),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Gestionar rol', 'url'=>array('admin')),
 );
 ?>
 
-<h1>View Role #<?php echo $model->ID; ?></h1>
+<h1>Ver rol #<?php echo $model->ID; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
