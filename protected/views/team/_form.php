1d2cf630018bdc6b5ce7ca2737346feb1a0bf468
lnatarenr <lnatarenr@gmail.com>
2014-10-27 01:55:59 +0000

    campos

--- protected/views/team/_form.php
+++ protected/views/team/_form.php
@@ -12,23 +12,26 @@
 	<?php echo $form->errorSummary($model); ?>
 
 	
-	<div class="span3"> 
+	<div class="span6"> 
 	
-	<?php echo $form->textFieldRow($model,'NAME',array('size'=>60,'maxlength'=>100,'class'=>'span12')); ?>
-	<?php echo $form->dropDownListRow($model,'ID_CATEGORY',CHtml::listData($catCategory::model()->findAll(),'ID_CATEGORY','NAME')); ?>
-	<?php echo $form->textAreaRow($model, 'DESCRIPTION', array('class'=>'span3', 'rows'=>5)); ?>
+	<?php echo $form->textFieldRow($model,'NAME',array('size'=>60,'maxlength'=>100,'class'=>'input-xlarge')); ?>
+	<?php echo $form->textFieldRow($model,'ADDRESS',array('size'=>60,'maxlength'=>100,'class'=>'input-xlarge')); ?>
+	<?php echo $form->textFieldRow($model,'EMAIL',array('size'=>60,'maxlength'=>100,'class'=>'input-xlarge')); ?>
+	
+	
 	</div>
-	<div class="span3"> 
-	<?php echo $form->textFieldRow($model,'ADDRESS',array('size'=>60,'maxlength'=>100,'class'=>'span12')); ?>
-	
-	<?php echo $form->textFieldRow($model,'EMAIL',array('size'=>60,'maxlength'=>100,'class'=>'span12')); ?>
+	<div class="span6"> 
+
 	<?php echo $form->dropDownListRow($model,'ACTIVE',$model->getEnabledOptions());?>
+	<?php echo $form->dropDownListRow($model,'ID_CATEGORY',CHtml::listData($catCategory::model()->findAll(),'ID_CATEGORY','NAME')); ?>
+	<?php echo $form->textAreaRow($model, 'DESCRIPTION', array('class'=>'span6', 'rows'=>5)); ?>
+	
 	</div>
 	</fieldset>
 	
 
 	<div class="form-actions">
-		<?php $this->widget('bootstrap.widgets.TbButton',array('buttonType'=>'submit','type'=>'primary','label'=>$model->isNewRecord?'Crear	':'Update'));?>
+		<?php $this->widget('bootstrap.widgets.TbButton',array('buttonType'=>'submit','type'=>'primary','label'=>$model->isNewRecord?'Crear	':'Actualizar'));?>
 		
 	</div>
 
