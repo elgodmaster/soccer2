1892b4f9e09252cab28d41097ebb4b9a39dce9f6
lnatarenr <lnatarenr@gmail.com>
2014-11-04 01:12:45 +0000

    cambios de texto

--- protected/views/document/_form.php
+++ protected/views/document/_form.php
@@ -2,7 +2,7 @@
 
 <?php $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
     'id'=>'verticalForm',
-    'htmlOptions'=>array('class'=>'well'),
+    'htmlOptions'=>array(),
 )); ?>
 
 	<p class="note">Los campos con:  <span class="required">*</span> son requeridos.</p>
@@ -10,21 +10,21 @@
 	<?php echo $form->errorSummary($model); ?>
 
 	<fieldset>
-		
-		<?php echo $form->textFieldRow($model,'NAME',array('size'=>60,'maxlength'=>100 , 'class'=>'span3')); ?>
+		<div class="span6">
+		<?php echo $form->textFieldRow($model,'NAME',array('size'=>60,'maxlength'=>100 , 'class'=>'input-xlarge')); ?>
 		
 		 <a href="#" rel="tooltip" title="A short name for document">?</a>
-	
+		<?php echo $form->textAreaRow($model, 'DESCRIPTION', array('class'=>'span6', 'rows'=>5)); ?>
 		
 		<?php echo $form->dropDownListRow($model, 'TYPE_DOCUMENT', $model->getTypeDocumentOptions()); ?>
 		
-		<?php echo $form->textAreaRow($model, 'DESCRIPTION', array('class'=>'span3', 'rows'=>5)); ?>
-
 	
+
+	</div>
 	</fieldset>	
 		
 		<div class="form-actions">
-		<?php $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit',  'type'=>'primary','label'=> $model->isNewRecord ?'Create' : 'Update')); ?>
+		<?php $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit',  'type'=>'primary','label'=> $model->isNewRecord ?'Crear' : 'Actualizar')); ?>
 		
 		 <?php //echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
 
