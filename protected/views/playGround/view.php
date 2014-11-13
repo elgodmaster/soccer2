4f9af88d15a7c1652119d556fe5addc9dda31683
lnatarenr <lnatarenr@gmail.com>
2014-11-13 22:41:59 +0000

    traduccion de textos

--- protected/views/playGround/view.php
+++ protected/views/playGround/view.php
@@ -8,15 +8,15 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'Lista descansso', 'url'=>array('index')),
-	array('label'=>'Obtener descanso', 'url'=>array('create')),
-	array('label'=>'Modificar descanso', 'url'=>array('update', 'id'=>$model->ID)),
-	array('label'=>'Borrar descanso', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->ID),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Lista canchas', 'url'=>array('index')),
+	array('label'=>'Obtener cancha', 'url'=>array('create')),
+	array('label'=>'Modificar cancha', 'url'=>array('update', 'id'=>$model->ID)),
+	array('label'=>'Borrar cancha', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->ID),'confirm'=>'Are you sure you want to delete this item?')),
 	
 );
 ?>
 
-<h2>Ver descansos #<?php echo $model->ID; ?></h2>
+<h2>Ver canchas #<?php echo $model->ID; ?></h2>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
