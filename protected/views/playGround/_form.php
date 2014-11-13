6ec7a5daf55217c14ea1cf6355569134e23c0779
lnatarenr <lnatarenr@gmail.com>
2014-11-13 22:30:07 +0000

    cambio de tamaño de los campos

--- protected/views/playGround/_form.php
+++ protected/views/playGround/_form.php
@@ -8,7 +8,7 @@
 
 <?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm', array(
 	'id'=>'verticalForm',
-	'htmlOptions'=>array('class'=>'well'),
+	'htmlOptions'=>array(),
 )); ?>
 
 	<p class="note">Los campos con: <span class="required">*</span> son requeridos.</p>
@@ -17,21 +17,25 @@
 
 	<fieldset>
 	
-		<?php echo $form->textFieldRow($model,'NAME',array('size'=>60,'maxlength'=>100, 'class'=>'spen3')); ?>
+	<div class="span6">
+		<?php echo $form->textFieldRow($model,'NAME',array('size'=>60,'maxlength'=>100,'class'=>'input-xlarge')); ?>
 		
 		<a href="#" rel="tooltip title="A short name for playground">?</a>
+		<?php echo $form->textFieldRow($model,'PHONE_NUMBER',array('size'=>60,'maxlength'=>100, 'class'=>'input-xlarge')); ?>
 	
-	<?php echo $form->textAreaRow($model, 'DESCRIPTION', array('class'=>'span3', 'rows'=>5)); ?>
-	<?php echo $form->textFieldRow($model,'ADDRESS',array('size'=>60,'maxlength'=>100, 'class'=>'spen3')); ?>
-	<?php echo $form->textFieldRow($model,'PHONE_NUMBER',array('size'=>60,'maxlength'=>100, 'class'=>'spen3')); ?>
+	<?php echo $form->textFieldRow($model,'ADDRESS',array('size'=>60,'maxlength'=>100, 'class'=>'input-xlarge')); ?>
+	</div>
+	<div class="span6">
+	
 	<?php echo $form->dropDownListRow($model, 'ACTIVE', $model->getActiveOptions()); ?>
-	<?php echo $form->dropDownListRow($model, 'MAP_STRING', array('size'=>60,'maxlength'=>100, 'class'=>'spen3')); ?>
-	
+	<?php echo $form->dropDownListRow($model, 'MAP_STRING', array('size'=>60,'maxlength'=>100,'class'=>'input-xlarge')); ?>
+	<?php echo $form->textAreaRow($model, 'DESCRIPTION', array('class'=>'span6', 'rows'=>5)); ?>
+	</div>
 	</fieldset>
 
 
 	<div class="form-actions">
-	<?php $this->widget('bootstrap.widgets.TbButton',array('buttonType'=>'submit','type'=>'primary','label'=>$model->isNewRecord?'Create':'Update'));?>
+	<?php $this->widget('bootstrap.widgets.TbButton',array('buttonType'=>'submit','type'=>'primary','label'=>$model->isNewRecord?'Crear':'Actualizar'));?>
 	
 	
 		<?php //echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
