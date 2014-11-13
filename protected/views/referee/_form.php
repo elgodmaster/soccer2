e8901dc12c577ae469217e727839ce8ed9a28083
lnatarenr <lnatarenr@gmail.com>
2014-11-13 21:49:26 +0000

    cambios de columnas de campos

--- protected/views/referee/_form.php
+++ protected/views/referee/_form.php
@@ -6,18 +6,24 @@
 	<p class="help-block">Los campos con: <span class="required">*</span> son requeridos.</p>
 
 	<?php echo $form->errorSummary($model); ?>
-
+	
+    <fieldset>
+    <div class="span6" >
 	<?php echo $form->textFieldRow($model,'NAME',array('class'=>'span5','maxlength'=>300)); ?>
-
+   
 	<?php echo $form->textFieldRow($model,'ADDRESS',array('class'=>'span5','maxlength'=>500)); ?>
-
+   
 	<?php echo $form->textFieldRow($model,'EMAIL',array('class'=>'span5','maxlength'=>300)); ?>
-
+    </div>
+    
+    <div class="span6">
 	<?php echo $form->textFieldRow($model,'PHONE',array('class'=>'span5','maxlength'=>50)); ?>
 
 	<?php echo $form->textFieldRow($model,'DATE_UP',array('class'=>'span5')); ?>
 
 	<?php echo $form->textFieldRow($model,'ACTIVE',array('class'=>'span5')); ?>
+	</div>
+	</fieldset>
 
 	<div class="form-actions">
 		<?php $this->widget('bootstrap.widgets.TbButton', array(
